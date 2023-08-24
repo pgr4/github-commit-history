bb24a5a03a7ca16dd04063a7d1c8dbef39682c35
patrick Ryan <iampgr4@gmail.com>
2023-08-24 05:09:37 +0000

    Added new Commits Component

---
+++ src/app/commits/commits.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commits',
+  templateUrl: './commits.component.html',
+  styleUrls: ['./commits.component.css']
+})
+export class CommitsComponent {
+
+}
